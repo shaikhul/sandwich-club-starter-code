2c9197499c2f16e620e8dadf52be637af704f558
Shaikhul Chowdhury <schowdhury@rsglab.com>
2018-04-27 01:43:31 -0400

    Populate detail activity using sandwich object

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +47,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +60,21 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView originTextView = findViewById(R.id.origin_tv);
+        originTextView.setText(sandwich.getPlaceOfOrigin());
 
+        TextView alsoKnownAsTextView = findViewById(R.id.also_known_tv);
+        for (String alias: sandwich.getAlsoKnownAs()) {
+            alsoKnownAsTextView.append(alias + ", ");
+        }
+
+        TextView ingredientsTextView = findViewById(R.id.ingredients_tv);
+        for (String ingredient: sandwich.getIngredients()) {
+            ingredientsTextView.append(ingredient + ", ");
+        }
+
+        TextView descriptionTextView = findViewById(R.id.description_tv);
+        descriptionTextView.setText(sandwich.getDescription());
     }
 }
