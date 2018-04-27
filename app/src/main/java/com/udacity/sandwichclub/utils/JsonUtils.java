158b7974fa7607df1db475da458c269aad24b51f
Shaikhul Chowdhury <schowdhury@rsglab.com>
2018-04-27 01:42:53 -0400

    Implement parseSandwichJson util method

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,44 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
+    private static ArrayList<String> convertJsonArrayToList(JSONArray list) throws JSONException {
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < list.length(); i++) {
+            arrayList.add(list.getString(i));
+        }
+        return arrayList;
+    }
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject nameObject = jsonObject.getJSONObject("name");
+
+            String mainName = nameObject.getString("mainName");
+            JSONArray alsoKnownAs = nameObject.getJSONArray("alsoKnownAs");
+            ArrayList<String> aliases = convertJsonArrayToList(alsoKnownAs);
+
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            String description = jsonObject.getString("description");
+            String image = jsonObject.getString("image");
+
+            JSONArray ingredients = jsonObject.getJSONArray("ingredients");
+            ArrayList<String> ingredientList = convertJsonArrayToList(ingredients);
+
+            Sandwich sandwich = new Sandwich(mainName, aliases, placeOfOrigin, description, image, ingredientList);
+            return sandwich;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
